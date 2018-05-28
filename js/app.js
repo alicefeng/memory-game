859ca391d3c826c71d81c6cc9dbba853869e8d26
Alice Feng <feng.a.g@gmail.com>
2018-05-28 13:43:09 -0400

    Clean up comments

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-/*
- * Create a list that holds all of your cards
- */
+/* code credits from the live webinar with Mike Wales */
+
+let openCards = [];
+let moves, matches, timeElapsed;
+const moveCounter = document.querySelector('.moves');
+const resetButton = document.querySelector('.restart');
+const starHTML = '<li><i class="fa fa-star"></i></li>';
+const starCounter = document.querySelector('.stars');
+const timer = document.querySelector('.timer');
+let newTime;
+const congratsPopup = document.querySelector('.congrats_popup');
+const finalStars = document.querySelector('.final_stars');
+const finalMoves = document.querySelector('.final_moves');
+const finalTime = document.querySelector('.final_time');
+const playAgainButton = document.querySelector('.play_again');
+
 const cards = ['fa-diamond', 'fa-diamond',
 			 'fa-paper-plane-o', 'fa-paper-plane-o',
 			 'fa-anchor', 'fa-anchor',
@@ -13,12 +26,6 @@ const cards = ['fa-diamond', 'fa-diamond',
 function generateCard(card) {
 	return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
 }
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -39,10 +46,10 @@ function showCard(card) {
 	card.classList.add('open', 'show');
 }
 
-// function to remove star
 function removeStar() {
 	let numStars = starCounter.childElementCount;
 
+	// if player already has no stars left, don't keep trying to remove more
 	if(numStars > 0) {
 		starCounter.removeChild(starCounter.firstElementChild);
 	}
@@ -64,7 +71,7 @@ function getNewTime() {
     timer.innerHTML = `${hours}`.padStart(2, '0') + ':' + `${minutes}`.padStart(2, '0') + ':' + `${seconds}`.padStart(2, '0');
 }
 
-// populate final popup with stats
+// populate final popup with stats (stars left, total moves, total time)
 function makeCongratsPopup() {
 	finalStars.innerText = starCounter.childElementCount;
 	finalMoves.innerText = moves;
@@ -73,45 +80,13 @@ function makeCongratsPopup() {
 	congratsPopup.classList.add('won');
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-// initialize game by setting board and resetting all counters
-function initGame() {
-	const deck = document.querySelector('.deck');
-	let shuffledCards = shuffle(cards);
-	let cardHTML = shuffledCards.map(function(card) {
-		return generateCard(card);
-	});
-	deck.innerHTML = cardHTML.join('');
-
-	moves = 0;
-	moveCounter.innerText = moves;
-
-	timeElapsed = 0;
-	timer.innerText = '00:00:00';
-
-	matches = 0;
-
-	resetStars(3);
-
-	playGame();
-}
-
 function playGame() {
 	const allCards = document.querySelectorAll('.card');
 
 	allCards.forEach(function(card) {
 		card.addEventListener('click', function(e) {
 
+			// only show cards that aren't already flipped and if there aren't already 2 cards showing
 			if (!card.classList.contains('open') && !card.classList.contains('show') && openCards.length <=1) {
 				openCards.push(card);
 				showCard(card);
@@ -125,12 +100,13 @@ function playGame() {
 						openCards[1].classList.add('match', 'animated', 'pulse');
 						showCard(openCards[1]);
 
+						// empty array of open cards after match so use can click on more cards
 						openCards = [];
 						matches += 1;
 					}
 
 					else {
-						// if no match, cards should flip back
+						// if no match, cards should flip back with a slight delay
 						setTimeout(function() {
 							openCards.forEach(function(card) {
 								card.classList.remove('open', 'show');
@@ -159,19 +135,27 @@ function playGame() {
 	});
 }
 
-let openCards = [];
-let moves, matches, timeElapsed;
-const moveCounter = document.querySelector('.moves');
-const resetButton = document.querySelector('.restart');
-const starHTML = '<li><i class="fa fa-star"></i></li>';
-const starCounter = document.querySelector('.stars');
-const timer = document.querySelector('.timer');
-let newTime;
-const congratsPopup = document.querySelector('.congrats_popup');
-const finalStars = document.querySelector('.final_stars');
-const finalMoves = document.querySelector('.final_moves');
-const finalTime = document.querySelector('.final_time');
-const playAgainButton = document.querySelector('.play_again');
+// initialize game by setting board and resetting all counters
+function initGame() {
+	const deck = document.querySelector('.deck');
+	let shuffledCards = shuffle(cards);
+	let cardHTML = shuffledCards.map(function(card) {
+		return generateCard(card);
+	});
+	deck.innerHTML = cardHTML.join('');
+
+	moves = 0;
+	moveCounter.innerText = moves;
+
+	timeElapsed = 0;
+	timer.innerText = '00:00:00';
+
+	matches = 0;
+
+	resetStars(3);
+
+	playGame();
+}
 
 initGame();
 newTime = setInterval(getNewTime, 1000);
